68df1c15a06a68ff7fd9ffe18e50bd4fd75a3f10
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-12-16 17:35:44 +0500

    fix skymarket

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -1,8 +1,12 @@
-from django.urls import include, path
+from django.urls import path, include
 
-# TODO настройка роутов для модели
+from ..ads.views import AdViewSet, CommentViewSet
+from rest_framework import routers
 
+ads_router = routers.SimpleRouter()
+ads_router.register('ads', AdViewSet)
+ads_router.register('ads/(?P<ad_id>[^/.]+)/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
-
+    path('', include(ads_router.urls)),
 ]
